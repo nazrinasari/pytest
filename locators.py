2255112506541a81a6b56fba3b4c8945c5a4f778
Nazrin Asari <queckey@gmail.com>
2024-06-03 22:40:56 +0800

    update

--- locators.py
+++ locators.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class LoginPageLocators:
-    EMAIL_FIELD = "user-name"
-    PASSWORD_FIELD = "password"
-    LOGIN_BUTTON = "login-button"
+    EMAIL_FIELD = 'user-name'
+    PASSWORD_FIELD = 'password'
+    LOGIN_BUTTON = 'login-button'
+
+class ProductPageLocators:
+    PRODUCTS = 'inventory_item'
+    ERROR = '[data-test="error"]'
